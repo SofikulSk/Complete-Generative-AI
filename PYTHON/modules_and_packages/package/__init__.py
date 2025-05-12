0154def5ce402bb5b63a09205c28b517c1b4f2bc
SOFIKUL SK <148935407+sofikulsk02@users.noreply.github.com>
2025-05-12 09:44:22 +0530

    Topic:NLP & TensorFlow

---
+++ PYTHON/modules_and_packages/package/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py is a special file in Python that indicates that the directory should be treated as a package.
+# It can be empty, or it can contain initialization code for the package.
